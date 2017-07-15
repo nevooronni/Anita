077025f889ec321bcd27f8b0f3c18bd539b56023
Neville Oronni <nevooronni@gmail.com>
2017-07-16 02:18:44 +0300

    added script file

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$("#breakfastb").mouseover(function(){
+    $("#breakfastb").css("background-color", "white");
+});
+
+$("#lunchb").mouseover(function(){
+    $("#lunchb").css("background-color", "white");
+});
+
+$("#dinnerb").mouseover(function(){
+    $("#dinnerb").css("background-color", "white");
+});
