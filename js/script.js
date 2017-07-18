98e5b3b103a017726a6a0dfd42ffecde1af031ba
Neville Oronni <nevooronni@gmail.com>
2017-07-18 20:20:03 +0300

    added comments

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-//Javascript file\
+//Javascript file
 
-/*open*/
+//open
 function openNav() {
 	document.getElementById("myNav").style.height = "100%";
 }
 
-/*close*/
+//close
 function closeNav() {
 	document.getElementById("myNav").style.height = "0%";
 }
