f658e2f073deacbfdf514a475efe16d6d19ceb0c
Neville Oronni <nevooronni@gmail.com>
2017-07-28 23:05:59 +0300

    added a subscribe confirmation pop up functionality in the subscribe section

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
-//Javascript file
+//Javascript/jquery file
 
+//js frontend
 //open
 function openNav() {
 	document.getElementById("myNav").style.height = "100%";
@@ -10,3 +11,27 @@ function closeNav() {
 	document.getElementById("myNav").style.height = "0%";
 }
 
+//jquery backend
+$(document).ready(function() {
+	$("input#submit").click(function() {
+    
+    //takes the input of the form and turns into to a string
+		var input = $("input#email").val();
+
+		//inpits the string onto our good old span element
+		$("span#email102").text(input);
+
+		//shows hidden subscribe confirmation
+		$("div#hiddenSubscribe").show();
+
+	  //when you click the popups dissapears 
+	  $("span#closebtn").click(function() {
+	  	$("div#hiddenSubscribe").hide();
+	  });
+	}) ;
+			
+});
+		
+  
+
+    
